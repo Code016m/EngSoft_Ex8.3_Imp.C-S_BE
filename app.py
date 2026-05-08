b7309fb460f3ac37f80a02632de5c1bd4dce8edd
Code016m <miguel.r.araujo16@gmail.com>
2026-05-08 16:36:12 -0300

    Refactor app.py to simplify home route response and clean up code formatting

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 import os
-
-from flask import Flask, redirect
+from flask import Flask
 from flask_cors import CORS
 
 from routes.livros import livros_bp
 
 app = Flask(__name__)
-
 CORS(app)
 
-app.register_blueprint(
-    livros_bp,
-    url_prefix="/livros"
-)
+app.register_blueprint(livros_bp, url_prefix="/livros")
 
 @app.route("/")
 def home():
-    return redirect("/livros/")
+    return {
+        "status": "API rodando com sucesso",
+        "service": "livraria"
+    }
+
+@app.route("/test")
+def test():
+    return {
+        "message": "Teste OK",
+        "status": 200
+    }
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
