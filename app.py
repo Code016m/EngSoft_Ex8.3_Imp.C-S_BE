90daf1e0580ec8da9d47df34a2093963d56bfd1e
Code016m <miguel.r.araujo16@gmail.com>
2026-05-08 13:49:51 -0300

    Improve formatting and readability in app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
+
 from routes.livros import livros_bp
 
 app = Flask(__name__)
+
 CORS(app)
 
-app.register_blueprint(livros_bp, url_prefix="/livros")
+app.register_blueprint(
+    livros_bp,
+    url_prefix="/livros"
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
