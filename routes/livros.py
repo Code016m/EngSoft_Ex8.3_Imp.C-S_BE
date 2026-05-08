693472786164cc831999c7e5c73f6063dcd06a75
Code016m <miguel.r.araujo16@gmail.com>
2026-05-08 13:51:11 -0300

    Refactor livros.py: improve function names, enhance error messages, and format code for clarity

--- routes/livros.py
+++ routes/livros.py
@@ -3,23 +3,29 @@ from models.livro import Livro
 
 livros_bp = Blueprint("livros", __name__)
 
+# banco em memória
 livros = []
 
 
-# GET - listar livros
+# GET /livros
 @livros_bp.route("/", methods=["GET"])
-def listar():
-    return jsonify([livro.to_dict() for livro in livros]), 200
+def listar_livros():
+
+    return jsonify(
+        [livro.to_dict() for livro in livros]
+    ), 200
 
 
-# POST - adicionar livro
+# POST /livros
 @livros_bp.route("/", methods=["POST"])
-def adicionar():
+def adicionar_livro():
 
     data = request.get_json()
 
     if not data:
-        return jsonify({"erro": "JSON inválido"}), 400
+        return jsonify({
+            "erro": "JSON inválido"
+        }), 400
 
     titulo = data.get("titulo")
     autor = data.get("autor")
@@ -27,7 +33,7 @@ def adicionar():
 
     if not titulo or not autor or preco is None:
         return jsonify({
-            "erro": "Campos obrigatórios: titulo, autor, preco"
+            "erro": "Campos obrigatórios"
         }), 400
 
     try:
@@ -35,18 +41,18 @@ def adicionar():
 
     except ValueError:
         return jsonify({
-            "erro": "Preço deve ser numérico"
+            "erro": "Preço inválido"
         }), 400
 
-    # cria objeto Livro
-    novo_livro = Livro(titulo, autor, preco)
+    novo_livro = Livro(
+        titulo,
+        autor,
+        preco
+    )
 
-    # salva objeto na lista
     livros.append(novo_livro)
 
-    print("Livro adicionado:", novo_livro.to_dict())
-
     return jsonify({
-        "msg": "Livro adicionado com sucesso",
+        "msg": "Livro cadastrado",
         "livro": novo_livro.to_dict()
     }), 201
